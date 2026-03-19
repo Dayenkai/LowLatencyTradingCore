88459ee7407c079bf193c85d7b2bb6cda67fb9fd
Dayenkai <BawiluEnt@hotmail.com>
2026-03-19 01:06:20 +0100

    Nic Thread can transmit batch events to feedHandler threads through SPCS ring buffers.

--- src/FeedHandler/feedHandler.cc
+++ src/FeedHandler/feedHandler.cc
@@ -8,12 +8,21 @@ int  feedHandler(MemoryPool &memory)
     log("__IN FEED_HANDLER__ ! Yeah !");
     while (true)
     {
-        if (!((memory.rxRingDesc.tail + 1) % RX_RING_SIZE == memory.rxRingDesc.head))
+        
+        if (!(((memory.rxRingDesc.tail) & (RX_RING_SIZE  - 1)) == memory.rxRingDesc.head ))
         {
+            //memory.rxRingDesc.tail.load(std::memory_order_acquire);
             memory.rxRingDesc.tail.load(std::memory_order_acquire);
-            memory.rxRingDesc.tail =  (memory.rxRingDesc.tail + 1) % (RX_RING_SIZE - 1);
             std::cout << "Readind an entry !";
-            memory.rxRingDesc.tail.load(std::memory_order_release);
+            for (size_t idx = 0; idx < memory.rxRingDesc.data[memory.rxRingDesc.tail].len; idx++)
+            {
+                std::cout << memory.rxRingDesc.data[memory.rxRingDesc.tail].addr[idx];
+            }
+            if (memory.rxRingDesc.data[memory.rxRingDesc.tail].len != 0)
+            {
+                std::cout << std::endl;
+            }
+            memory.rxRingDesc.tail.store((memory.rxRingDesc.tail + 1) & (RX_RING_SIZE - 1), std::memory_order_release);
         }
     }
     return 0;
--- src/NIC/nicReplay.cc
+++ src/NIC/nicReplay.cc
@@ -7,22 +7,24 @@ int     NicReplay(std::vector<ChannelFile>   &channelContent, std::vector<Memory
     //std::cout << "File processed by Nic with content : " << channelContent << std::endl;
 
     std::string         token("");
-    uint64_t            packetCount(0);
     std::string         line;
 
     for (uint64_t i = 0; i < channelContent.size(); i++)
     {
         std::stringstream   channel_Content_Stream(channelContent[i]._channelContent);
+        uint64_t            packetCount(0);
 
-        while(std::getline(channel_Content_Stream, line) && ((memoryPool[i].rxRingDesc.head + 1) & RX_RING_SIZE - 1) != memoryPool[i].rxRingDesc.tail)
+        
+        while(std::getline(channel_Content_Stream, line) && ((memoryPool[i].rxRingDesc.head + 1) & (RX_RING_SIZE - 1)) != memoryPool[i].rxRingDesc.tail)
         {
+            //memoryPool[i].rxRingDesc.head.load(std::memory_order_acquire);
+            
             memoryPool[i].rxRingDesc.head.load(std::memory_order_acquire);
-            std::cout << "Writing an entry !!!" << std::endl;
             memcpy(memoryPool[i].arena[packetCount], line.c_str(), line.length());
             memoryPool[i].rxRingDesc.data[packetCount & (RX_RING_SIZE - 1)].addr = memoryPool[i].arena[packetCount];
             memoryPool[i].rxRingDesc.data[packetCount & (RX_RING_SIZE - 1)].len  = line.length();
-            memoryPool[i].rxRingDesc.head++;
-            memoryPool[i].rxRingDesc.head.load(std::memory_order_release);
+            ++packetCount;
+            memoryPool[i].rxRingDesc.head.store((memoryPool[i].rxRingDesc.head + 1) & (RX_RING_SIZE - 1), std::memory_order_release);
         }
     }
     // std::stringstream   channel_Content_Stream(channelContent);
